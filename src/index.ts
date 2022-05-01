04bb8bb52424d308a83760fbc5035043e3ee087e
Izik AJ <izikaj@gmail.com>
2022-05-01 20:55:36 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { Signer } from "./signer";
-import { Builder } from "./builder";
-import * as Utils from "./utils";
+import { Signer } from './signer';
+import { Builder } from './builder';
+import * as Utils from './utils';
 
-export {
-  Signer,
-  Builder,
-  Utils
-};
+export { Signer, Builder, Utils };
